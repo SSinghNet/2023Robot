b5d663112eef446e2b10bed19062a9a85be18f89
Michael <michaeljpanny@gmail.com>
2023-02-07 15:52:11 -0500

    MovePosSpeed cmd actually works now

--- src/main/java/org/mort11/commands/elevator/MoveElevatorSpeed.java
+++ src/main/java/org/mort11/commands/elevator/MoveElevatorSpeed.java
@@ -28,9 +28,7 @@ public class MoveElevatorSpeed extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if(speed == 0){
-      System.out.println("In");
-    }
+    elevator.move(speed);
   }
 
   // Called once the command ends or is interrupted.
