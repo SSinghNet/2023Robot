40102fcc9808662c533d298234c8865a68e60263
Sumeet Singh <sumeet605@gmail.com>
2023-02-08 10:16:28 -0500

    random

--- src/main/java/org/mort11/commands/auto/Balance.java
+++ src/main/java/org/mort11/commands/auto/Balance.java
@@ -5,8 +5,8 @@ import org.mort11.subsystems.Drivetrain;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-public class Balance extends CommandBase{
-    private Drivetrain drivetrain;
+public class Balance extends CommandBase {
+	private Drivetrain drivetrain;
 
 	public Balance() {
 		drivetrain = Drivetrain.getInstance();
@@ -22,14 +22,12 @@ public class Balance extends CommandBase{
 	@Override
 	public void execute() {
 
-		if(Math.abs(drivetrain.getNavX().getPitch()) > 0.5){
-			drivetrain.drive(new ChassisSpeeds(
-				0, drivetrain.getBalanceController().calculate(-drivetrain.getNavX().getPitch()), 0
-			));
+		if (Math.abs(drivetrain.getNavX().getPitch()) > 0.5) {
+			drivetrain.drive(new ChassisSpeeds(0,
+					drivetrain.getBalanceController().calculate(-drivetrain.getNavX().getPitch()), 0));
 		} else if (Math.abs(drivetrain.getNavX().getRoll()) > 0.5) {
 			drivetrain.drive(new ChassisSpeeds(
-					drivetrain.getBalanceController().calculate(-drivetrain.getNavX().getRoll()), 0, 0
-			));
+					drivetrain.getBalanceController().calculate(-drivetrain.getNavX().getRoll()), 0, 0));
 		}
 
 	}
